acbd97be36aee23da7248a9333cc12e0f085243a
keidavey <keindavey@gmail.com>
2025-01-15 18:40:12 -0500

    paths fixed

--- src/test/java/com/cicd/Cucumber_BDD_Testing/runners/Run_Smoke_Test.java
+++ src/test/java/com/cicd/Cucumber_BDD_Testing/runners/Run_Smoke_Test.java
@@ -6,8 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(plugin = {"html:target/cucumber-reports/cucumber-html-report.html", "json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-        features = "src/test/java/com/chromatech/Cucumber_BDD_Testing/features",
-        glue = {"com.chromatech.Cucumber_BDD_Testing.stepDefinitions", "hooks"},
+        features = "src/test/java/com/cicd/Cucumber_BDD_Testing/features",
+        glue = {"com.cicd.Cucumber_BDD_Testing.stepDefinitions", "hooks"},
         tags = "@Smoke",
         dryRun = false
 )
