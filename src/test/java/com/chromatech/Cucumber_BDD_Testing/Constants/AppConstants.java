20181aa2024a3a020a0eeaaea63837b69a58b810
keidavey <keindavey@gmail.com>
2025-01-14 22:46:47 -0500

    boop

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/Constants/AppConstants.java
+++
@@ -1,7 +0,0 @@
-package com.chromatech.Cucumber_BDD_Testing.Constants;
-
-public class AppConstants {
-
-    public static String photoFilepath = System.getProperty("user.dir") + "/src/test/java/com/chromatech/Cucumber_BDD_Testing/Constants/famphoto.jpg";
-
-}
--- src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/PageInitializer.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/PageInitializer.java
@@ -9,27 +9,9 @@ public class PageInitializer {
      * PAGE OBJECTS
      */
 
-    public static StudentAdmissionPage studentAdmissionPage;
-    public static DashboardPage dashboardPage;
-    public static StudentDetailsPage studentDetailsPage;
-    public static StudentEditPage studentEditPage;
-    public static BulkDeletePage bulkDeletePage;
-    public static AddAndDeleteSectionsPage addAndDeleteSectionsPage;
-    public static ClassesPage classesPage;
-    public static CategoryPage categoryPage;
-    public static DisabledStudentPage disabledStudentPage;
     public static GooglePage googlePage;
 
     public static void initializeAllPages() {
-        studentAdmissionPage = new StudentAdmissionPage();
-        dashboardPage = new DashboardPage();
-        studentDetailsPage = new StudentDetailsPage();
-        studentEditPage = new StudentEditPage();
-        bulkDeletePage = new BulkDeletePage();
-        addAndDeleteSectionsPage = new AddAndDeleteSectionsPage();
-        classesPage = new ClassesPage();
-        categoryPage = new CategoryPage();
-        disabledStudentPage = new DisabledStudentPage();
         googlePage = new GooglePage();
     }
 }
