fd0d0ead9eb38b4347ec99fc3c35beab681db093
Katherine Davey <56743626+keidavey@users.noreply.github.com>
2025-01-15 04:40:01 -0500

    Update Run_CTSMS_Smoke_Test.java

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/runners/Run_CTSMS_Smoke_Test.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/runners/Run_CTSMS_Smoke_Test.java
@@ -3,7 +3,7 @@ package com.chromatech.Cucumber_BDD_Testing.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"html:target/cucumber-reports", "json:target/cucumber.json",
+@CucumberOptions(plugin = {"html:target/cucumber-reports/cucumber-html-report.html", "json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = "src/test/java/com/chromatech/Cucumber_BDD_Testing/features",
