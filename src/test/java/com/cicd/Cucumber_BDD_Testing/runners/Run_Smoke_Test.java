212a2f51141bdd08f589cb0856e24556077016b8
keidavey <keindavey@gmail.com>
2025-01-16 23:43:47 -0500

    index name change reports

--- src/test/java/com/cicd/Cucumber_BDD_Testing/runners/Run_Smoke_Test.java
+++ src/test/java/com/cicd/Cucumber_BDD_Testing/runners/Run_Smoke_Test.java
@@ -3,7 +3,7 @@ package com.cicd.Cucumber_BDD_Testing.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = {"html:target/cucumber-reports/cucumber-html-report.html", "json:target/cucumber.json",
+@CucumberOptions(plugin = {"html:target/cucumber-reports/index.html", "json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = "src/test/java/com/cicd/Cucumber_BDD_Testing/features",
